8d46a018349842d5262938d496e06b44c70f8feb
teklu <teklufa@gmail.com>
2023-09-12 16:52:45 +0300

    bootstrap sequelize
    run npx sequelize-cli init, to initialze the sequelize
    install dotenv
    configure db connection in .env and config/config.js file

---
+++ config/config.js
@@ -0,0 +1,17 @@
+import 'dotenv/config'
+export default {
+  "development": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": pocess.env.DB_DIALECT
+  },
+  "production": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": pocess.env.DB_DIALECT
+  }
+}
