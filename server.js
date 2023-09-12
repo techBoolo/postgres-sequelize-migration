50da6429f383f5fee91a119f51465609b924d68d
teklu <teklufa@gmail.com>
2023-09-12 17:25:59 +0300

    db connection
    use Sequelize to connect to the db
    add db connection logic before starting the server

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 import http from 'http'
 import app from './app.js'
+import db from './config/db.js'
 
 const PORT = process.env.PORT || 3001
 const server = http.createServer()
 
 server.on('request', app)
-
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+server.on('exit', async () => {
+  await db.close()
 })
+
+db.authenticate()
+  .then(() => {
+    console.log('Connected to db');
+    server.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    })
+  }, (err) => {
+    console.log(err);
+    console.log('db connection error', err.messags);
+    process.exit(1)
+  })
